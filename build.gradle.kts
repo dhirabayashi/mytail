e2d7041d9c3bc3993465a10b1e81befa979f9e05
daiki <gigabowser@hotmail.co.jp>
2022-07-06 20:31:54 +0900

    GraalVMの設定を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("application")
+    id("com.palantir.graal") version "0.12.0"
 }
 
 group = "org.example"
@@ -13,6 +15,8 @@ dependencies {
     implementation("info.picocli:picocli:4.6.3")
     implementation("org.apache.commons:commons-lang3:3.12.0")
 
+    annotationProcessor("info.picocli:picocli-codegen:4.6.3")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
@@ -21,4 +25,26 @@ dependencies {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+val jar by tasks.getting(Jar::class) {
+    manifest {
+        attributes["Main-Class"] = "com.github.dhirabayashi.mytail.MyTail"
+    }
+
+    from(
+            configurations.runtimeClasspath.get().map {
+                if (it.isDirectory) it else zipTree(it)
+            }
+    )
+    exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+}
+
+graal {
+    mainClass("com.github.dhirabayashi.mytail.MyTail")
+    outputName("mytail")
+    graalVersion("22.1.0")
+    javaVersion("17")
+    windowsVsVersion("2022")
+    windowsVsEdition("BuildTools")
 }
